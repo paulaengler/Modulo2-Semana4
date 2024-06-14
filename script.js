7a4008a39a8e10c10c33bfa3605d7a75a9d14ede
paulaengler <paulaengler@live.com>
2024-06-14 13:16:38 -0300

    ATUAL

---
+++ script.js
@@ -0,0 +1,33 @@
+function compartilharInstagram() {
+    // Lógica para compartilhar no Instagram
+    console.log('Compartilhar no Instagram');
+    window.open('https://www.instagram.com', '_blank');
+}
+
+function compartilharFacebook() {
+    // Lógica para compartilhar no Facebook
+    console.log('Compartilhar no Facebook');
+    window.open('https://www.facebook.com', '_blank');
+}
+
+function compartilharTwitter() {
+    // Lógica para compartilhar no Twitter
+    console.log('Compartilhar no Twitter');
+    window.open('https://twitter.com', '_blank');
+}
+
+function compartilhar(){
+    window.alert('Página em construção')
+}
+
+function assistirEpisodio1() {
+    console.log('Episódio 1 selecionado')
+}
+
+function assistirEpisodio2() {
+    console.log('Episódio 2 selecionado')
+}
+
+function assistirEpisodio3() {
+    console.log('Episódio 3 selecionado')
+}
